7b24476dd307866fe44ada7e7abb5cb05257b798
Ivson Santos <santosivson@hotmail.com>
2018-04-15 17:49:44 -0300

    Testando a primeira classe de dominio 'Categoria'

--- src/main/java/com/ivson/modelagemconceitual/resources/CategoriaResource.java
+++ src/main/java/com/ivson/modelagemconceitual/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.ivson.modelagemconceitual.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ivson.modelagemconceitual.model.Categoria;
 
 /**
  * PACKAGE resource = nome padrao para os recursos em uma API
@@ -13,8 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/categorias")	// nome do Endpoint REST, por padrao de mercado no plural
 public class CategoriaResource {
 
+	/**
+	 * OP spring Boot ja tem um conversor automatico que transforma qqr objeto em um JSON
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
