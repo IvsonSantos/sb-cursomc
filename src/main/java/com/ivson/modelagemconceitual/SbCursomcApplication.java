b47edbbf1be8b20c074dd296d8d8bd4a2134ede3
Ivson Santos <santosivson@hotmail.com>
2018-04-15 17:02:13 -0300

    Projeto Criado

---
+++ src/main/java/com/ivson/modelagemconceitual/SbCursomcApplication.java
@@ -0,0 +1,12 @@
+package com.ivson.modelagemconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCursomcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ivson/modelagemconceitual/SbCursomcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ivson.modelagemconceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbCursomcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
